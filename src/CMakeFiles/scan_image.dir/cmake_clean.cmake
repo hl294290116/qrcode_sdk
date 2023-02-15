9efc9900029d73fa6abb4e798859f637a4834e91
hanlei <aibee@hanlei.local>
2023-02-15 10:53:18 +0800

    delete cmake files

--- src/CMakeFiles/scan_image.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/scan_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/CMakeFiles/zbar.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libzbar.pdb"
-  "libzbar.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/zbar.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
